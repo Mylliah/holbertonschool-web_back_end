a35d5e6a7c264b1cbed361479b47841df7fcc3a9
Mylliah <mezhoud.my@gmail.com>
2025-07-28 16:35:47 +0200

    Add basic addition function and main script for testing

---
+++ 0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module contenant des fonctions d'addition de base."""
+
+
+def add(a: float, b: float) -> float:
+    """Retourne la somme de deux nombres flottants."""
+    return a + b
