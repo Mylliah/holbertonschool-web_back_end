3f9951a88eab13b46bab6d13b56d16bf50d32cca
Mylliah <mezhoud.my@gmail.com>
2025-07-31 12:28:27 +0200

    Add docstrings for module and wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""Basic async syntax module."""
 
 import random
 import asyncio
 
 
 async def wait_random(max_delay=10):
+    """Wait for a random delay between 0 and max_delay seconds."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
