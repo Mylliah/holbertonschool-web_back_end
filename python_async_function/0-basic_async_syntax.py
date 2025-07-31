a8e3e528f2f6906116cebde8e0016892cb1ed7a6
Mylliah <mezhoud.my@gmail.com>
2025-07-31 12:13:02 +0200

    Implement async function wait_random with random delay and add main script for testing

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
