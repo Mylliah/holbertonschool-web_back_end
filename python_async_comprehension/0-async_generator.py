611fdaff791f7bb5c5991478ca3f242b9130eeb3
Mylliah <mezhoud.my@gmail.com>
2025-08-01 12:29:56 +0200

    Fix async for loop in async_generator to ensure proper asynchronous iteration

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ import random
 
 async def async_generator():
     """Yields 10 random numbers between 0-10 with 1s delay."""
-    for i in range(10):
+    async for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
