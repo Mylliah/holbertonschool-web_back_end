d207b055186091a95ab4717e7c284cd9d5526b06
Mylliah <mezhoud.my@gmail.com>
2025-08-12 14:29:12 +0200

    Fix spacing in index_range function for consistent pagination calculations

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Module for helper functions for pagination."""
+
 
 def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of start and end indexes for pagination."""
-    start = (page -1) * page_size
+    start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -33,6 +33,6 @@ class Server:
 
 def index_range(page: int, page_size: int) -> tuple:
     """Calculates the start and end interval for pagination."""
-    start = (page -1) * page_size
+    start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
