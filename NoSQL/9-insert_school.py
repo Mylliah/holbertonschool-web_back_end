870f3eb6c6c61c3182d41e90075eeb69b18a7550
Mylliah <mezhoud.my@gmail.com>
2025-08-15 14:00:54 +0200

    Add docstrings to insert_school function and module for clarity

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""Module to insert a document into a MongoDB collection and return its _id."""
 
 def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document into the collection and returns its _id."""
     insertions = mongo_collection.insert_one(kwargs)
     return insertions.inserted_id
