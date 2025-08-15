35ee44cc4e2dc15e59e61543443bf4abfb0496ba
Mylliah <mezhoud.my@gmail.com>
2025-08-15 14:37:34 +0200

    Add main script and update_topics module for school topic management

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module to update the topics of a school document in a MongoDB collection"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates the topics of a school document based on the name"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
