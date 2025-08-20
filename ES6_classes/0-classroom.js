6f0e855c4458738beedf56ff1dabae19de91b84e
Mylliah <mezhoud.my@gmail.com>
2025-08-20 12:38:05 +0200

    fix: add class description comment to ClassRoom

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,3 +1,4 @@
+// ClassRoom represents a classroom with a maximum number of students.
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
