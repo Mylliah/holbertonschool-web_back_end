712fc9ae598cf5f58e9a05fc221035aa5a968100
Mylliah <mezhoud.my@gmail.com>
2025-08-20 18:56:59 +0200

    feat: implement Pricing class with validation for amount and currency; add main file for testing

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,38 +1,38 @@
 export default class Currency {
-    constructor(code, name) {
-        if (typeof code !== 'string') {
-            throw new TypeError("Code must be a string");
-        }
-        if (typeof name !== 'string') {
-            throw new TypeError("Name must be a string");
-        }
-        this._code = code;
-        this._name = name;
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError("Code must be a string");
-        }
-        this._code = value;
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
     }
+    this._code = value;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError("Name must be a string");
-        }
-        this._name = value;
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = value;
+  }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
