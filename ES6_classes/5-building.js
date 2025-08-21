93d8420cc29e561f7b0e69a3e6fd7f5c16688f3a
Mylliah <mezhoud.my@gmail.com>
2025-08-21 12:59:07 +0200

    fix: remove type check for sqft in constructor to simplify input validation

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
     this._sqft = sqft;
   }
 
