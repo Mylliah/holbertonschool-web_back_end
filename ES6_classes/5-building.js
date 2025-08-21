79384a55bb7fa69915a505a6ca44db36bd6ea2f9
Mylliah <mezhoud.my@gmail.com>
2025-08-21 12:53:02 +0200

    fix: move sqft type check to setter for improved validation

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
     this._sqft = sqft;
   }
 
@@ -10,7 +7,14 @@ export default class Building {
     return this._sqft;
   }
 
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a number')
+    }
+    this._sqft = value;
+  }
+
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    throw new TypeError('Class extending Building must override evacuationWarningMessage');
   }
 }
