f61ff4d65965622095047fb282d4bcbd94a79989
Mylliah <mezhoud.my@gmail.com>
2025-08-21 12:41:42 +0200

    refactor: convert HolbertonCourse to class syntax for improved structure and readability

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,59 +1,49 @@
-function HolbertonCourse(name, length, students) {
-  if (typeof name !== 'string') {
-    throw new TypeError('name must be a string');
-  }
-  if (typeof length !== 'number') {
-    throw new TypeError('length must be a number');
-  }
-  if (!Array.isArray(students) || !students.every(function (s) { return typeof s === 'string'; })) {
-    throw new TypeError('students must be an array of strings');
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
-  this._name = name;
-  this._length = length;
-  this._students = students;
-}
-
-Object.defineProperty(HolbertonCourse.prototype, 'name', {
-  get: function () {
+  get name() {
     return this._name;
-  },
-  set: function (value) {
+  }
+
+  set name(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('name must be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
-  },
-  enumerable: false,
-  configurable: true,
-});
+  }
 
-Object.defineProperty(HolbertonCourse.prototype, 'length', {
-  get: function () {
+  get length() {
     return this._length;
-  },
-  set: function (value) {
+  }
+
+  set length(value) {
     if (typeof value !== 'number') {
-      throw new TypeError('length must be a number');
+      throw new TypeError('Length must be a number');
     }
     this._length = value;
-  },
-  enumerable: false,
-  configurable: true,
-});
+  }
 
-Object.defineProperty(HolbertonCourse.prototype, 'students', {
-  get: function () {
+  get students() {
     return this._students;
-  },
-  set: function (value) {
-    if (!Array.isArray(value) || !value.every(function (s) { return typeof s === 'string'; })) {
-      throw new TypeError('students must be an array of strings');
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || !value.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
-  },
-  enumerable: false,
-  configurable: true,
-});
-
-export default HolbertonCourse;
+  }
+}
