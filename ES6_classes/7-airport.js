24cbc24b12beb394e723c940ec063caf858c0e3f
Mylliah <mezhoud.my@gmail.com>
2025-08-21 10:24:11 +0200

    feat: add toString method to Airport class for improved string representation

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -29,4 +29,8 @@ export default class Airport {
   [Symbol.for('nodejs.util.inspect.custom')]() {
     return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
   }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
