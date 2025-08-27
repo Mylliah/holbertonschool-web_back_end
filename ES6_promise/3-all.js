aa0198184d7a48b8a1f0e09c31d44e0ed2bec7a1
Mylliah <mezhoud.my@gmail.com>
2025-08-27 12:09:27 +0200

    feat: implement handleProfileSignup function and main file for execution

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ])
+    .then(([photo, user]) => { /* déstructuration en paramètres */
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
