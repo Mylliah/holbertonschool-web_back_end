eaf21541a7a08a06e6c1f5b25fac79fc77778267
Mylliah <mezhoud.my@gmail.com>
2025-08-27 16:15:12 +0200

    fix: correct import paths and simplify result mapping in handleProfileSignup function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,22 +1,14 @@
-import signUpUser from './4-user-promise.py';
-import uploadPhoto from './5-photo-reject.py';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      } else {
-        return {
-          status: result.status,
-          value: result.reason,
-        };
-      }
-    }));
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason,
+    }))
+  );
 }
