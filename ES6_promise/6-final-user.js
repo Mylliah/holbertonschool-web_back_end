2eb71cfc823321828f43db9527d6e6ed2ebf7434
Mylliah <mezhoud.my@gmail.com>
2025-08-27 16:25:04 +0200

    refactor: streamline result mapping in handleProfileSignup function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,16 +6,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      }
-      return {
-        status: result.status,
-        value: result.reason.toString,
-      };
-    }));
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
