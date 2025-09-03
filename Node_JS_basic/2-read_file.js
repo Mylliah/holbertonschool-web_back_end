413fcd1820eaefc46029b1a8bdf95b1ce630a66b
Mylliah <mezhoud.my@gmail.com>
2025-09-03 16:52:52 +0200

    fix: correct variable naming and formatting in countStudents function

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,22 +2,22 @@ const fs = require('fs');
 
 function countStudents(filePath) {
   try {
-    const data = fs.readFileSync(filePath, 'utf-8'); // on lit le fichier
+    const data = fs.readFileSync(filePath, 'utf8'); // on lit le fichier
     const lines = data.trim().split('\n'); // trim retire vides déb/fin | split découpe en tableau
     const headers = lines.shift().split(','); // shift récup 1ère ligne | split découpe en colonnes
     const students = lines.map((line) => { // map transforme chaque ligne en objet
-      const [firstName, field] = line.split(','); // destructuration -> affecte dans 2 variables
-      return { firstName, field }; // return objet students = { firstname: "str", field: "str" },..
+      const [firstname, , , field] = line.split(','); // destructuration -> affecte dans 2 variables
+      return { firstname, field }; // return objet students = { firstname: "str", field: "str" },..
     });
 
     console.log(`Number of students: ${students.length}`);
 
     const groups = {}; // prep dico pour regrouper par spé (clé = fiel, valeur = prénom)
     students.forEach((s) => { // comptage pour chaque student
-      if (!groups[s.field]) { // si clé n'existe pas
+      if (!groups[s.field]) { // si clé n'existe pas initialise à zéro
         groups[s.field] = []; // initialise à []
       }
-      groups[s.field].push(s.firstName); // sinon on ajoute prénom au tableau correspondant
+      groups[s.field].push(s.firstname); // sinon on ajoute prénom au tableau correspondant
     }); // cad CS : prénoms... | SWE : prénoms...
 
     for (const field in groups) {
