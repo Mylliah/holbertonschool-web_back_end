c5dd98cb509fa9c21620801e126b5897418cf42b
Mylliah <mezhoud.my@gmail.com>
2025-09-04 14:16:49 +0200

    feat: implement HTTP server to handle requests and respond with student data

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const DB_PATH = process.argv[2]; // le chemin est passé en argument
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === './students') {
+    res.write('This is the list of our students\n');
+
+    countStudents(DB_PATH)
+      .then(() => {
+        res.end();
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
+  } else {
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
