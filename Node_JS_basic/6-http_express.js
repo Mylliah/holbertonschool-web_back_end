2c4902fe7733f5365e5200d91a50017f110654ff
Mylliah <mezhoud.my@gmail.com>
2025-09-05 15:16:28 +0200

    feat: create HTTP server to respond with a greeting message

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Hoberton School');
+});
+
+app.listen(1245);
+
+module.exports = app;
