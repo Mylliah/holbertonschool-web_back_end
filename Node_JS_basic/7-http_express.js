2c926a4c9e7a6c19dd9026ab6394eb8e69d5b3d4
Mylliah <mezhoud.my@gmail.com>
2025-09-05 16:58:59 +0200

    feat: implement HTTP server with greeting and student list endpoints

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const DB_PATH = process.argv[2];
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+
+app.get('/students', (req, res) => {
+    res.set('Content-Type', 'text/plain');
+    countStudents(DB_PATH)
+        .then((data) => {
+            res.send(`This is the list of our students\n${data}`);
+        })
+        .catch((err) => {
+            res.end(`This is the list of our students\n${err.message}`);
+        });
+});
+
+app.listen(1245);
+
+module.exports = app;
